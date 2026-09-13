f969da9f14e452d75beb0881ad086d64d541ca32
siddharthanagula3 <nagulasiddhartha2@gmail.com>
2026-09-13 02:18:15 -0500

    refactor(vscode): delete the unread command registry scaffold

--- apps/extension-vscode/src/core/index.ts
+++ apps/extension-vscode/src/core/index.ts
@@ -2,12 +2,6 @@ export { validateAdvancedFeatureFlags, hasInlineCompletionCredential } from './a
 export { buildExtensionStatusBarText } from './statusBar';
 export { setupChat, type ChatState } from './chatSetup';
 export { setupCommands, type CommandDeps } from './commandSetup';
-export {
-  registerCommands,
-  type Command,
-  type CommandDeps as RegistryCommandDeps,
-  REGISTRY_COMMANDS,
-} from './commands';
 export { setupProviders, type ProviderState } from './providerSetup';
 export { runInlineCommand, commandLabel, type InlineCommand } from './runInlineCommand';
 export {
